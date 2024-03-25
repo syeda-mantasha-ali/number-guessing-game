dfbbde440417ea4b2133d51d5a337a96d2369f33
Syeda Mantasha Ali <syedamantashaali6@gmail.com>
2024-03-26 00:49:26 +0500

    initial commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+#! usr/bin/env node
+
+import inquirer from "inquirer"
+
+let number = Math.floor( Math.random() *10);
+
+const answers = await inquirer.prompt([
+    {message: "Enter Your Number", name:"userNumber", type:"number"}
+]);
+
+if (number === answers.userNumber) {
+    console.log('Congratulation! you guessed correct number');
+} else { 
+    console.log('Oops! Try Again');
+    console.log("The number was " + number);
+}
